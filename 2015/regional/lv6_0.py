22adec39f9093b5381c3855f6ff717253e93647f
cepalle <epalle.cyrille@gmail.com>
2020-03-11 14:39:24 +0100

    add 6_0 2015 perf

--- 2015/regional/lv6_0.py
+++ 2015/regional/lv6_0.py
@@ -1,101 +1,36 @@
-from random import randint
-
-
-def is_good(t, bn):
-    for i in range(0, bn[1]):
-        if (int(bn[0] / (2 ** (bn[1] - i - 1))) % 2 and
-                t[i] < bn[1] and
-                int(bn[0] / (2 ** (bn[1] - t[i] - 1))) % 2
-        ):
-            return False
-    return True
-
-
-def cal_bn(bn):
-    sum = 0
-    while (bn[0]):
-        if (bn[0] % 2):
-            sum += 1
-        bn[0] = int(bn[0] / 2)
-    return sum
-
-
-def arche_minus0_aux(n, t):
-    l_bn = [[0, 1], [1, 1]]
-    mx = 0
-    for bn in l_bn:
-        if (bn[1] == n):
-            mx = max(mx, cal_bn(bn))
-        else:
-            l_bn.append([bn[0] * 2, bn[1] + 1])
-            if (is_good(t, [bn[0] * 2 + 1, bn[1] + 1])):
-                l_bn.append([bn[0] * 2 + 1, bn[1] + 1])
-
-    return mx
-
-
-# -----------------------------
-
-def prt(tab):
-    for l in tab:
-        for elt in l:
-            print(elt, end="")
-            print(" ", end="")
-        print()
-
-
-def rm_i(M, i):
-    M.pop(i)
-    for l in M:
-        l.pop(i)
-
-
-def arche_minus0(n, t):
-    l = [0] * n
-    M = []
-    for i in range(0, n):
-        M.append(list(l))
-    for i in range(0, n):
-        M[t[i]][i] = 1
-        M[i][t[i]] = 1
-
-    sup_1 = True
-    sup_2 = True
-    while (sup_1 or sup_2):
-        # prt(M)
-        # print()
-        sup_1 = False
-        sup_2 = False
-        for i in range(0, len(M)):
-            if (sum(M[i]) == 1):
-                sup_1 = True
-                for j in range(0, len(M[i])):
-                    if (M[i][j]):
-                        rm_i(M, j)
-                        break
-                break
-
-        if (not sup_1):
-            for i in range(0, len(M)):
-                if (sum(M[i]) == 2):
-                    sup_2 = True
-                    rm_i(M, i)
-                    break
-
-    return len(M)
-
-
 n = int(input())
-t = list(map(int, input().split()))
+ln = [int(e) for e in input().split()]
 
-# n = 20
-# t = []
-# for i in range(0, n):
-#    t.append(randint(0, n - 1))
+lg = [set() for _ in range(0, n)]
+for i in range(0, n):
+    lg[ln[i]].add(i)
+    lg[i].add(ln[i])
 
-# print(n, t)
-# print()
+lg2 = []
+for i in range(0, n):
+    lg2.append((i, lg[i]))
 
+lg2.sort(key=lambda e: -len(e[1]))
 
-print("%d\n" % (arche_minus0(n, t)), end='')
-# print("%d\n" % ( arche_minus0_aux(n, t) ), end='')
+tot = 0
+
+while len(lg2) > 0:
+    si = set()
+    ss = set()
+    for (i, s) in lg2:
+        if len(s) == 1 and i not in ss:
+            si.add(i)
+            ss |= s
+
+    if len(si) == 0:
+        if len(lg2) > 0 and len(lg2[-1][1]) > 2:
+            lg2.sort(key=lambda c: -len(c[1]))
+        i, s = lg2[-1]
+        si.add(i)
+        ss = s
+
+    lg2 = list(map(lambda e: (e[0], e[1] - ss), filter(lambda e: e[0] not in si and len(si & e[1]) == 0, lg2)))
+
+    tot += len(si)
+
+print(tot)
