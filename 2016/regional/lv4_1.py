5622bdba2a8c684922924ed02ce27262075ca7de
cepalle <epalle.cyrille@gmail.com>
2020-03-11 15:32:36 +0100

    add 4_1 2016 perf

--- 2016/regional/lv4_1.py
+++ 2016/regional/lv4_1.py
@@ -1,52 +1,34 @@
-class hyst:
-    tab = []
-
-
-def traversee_du_desert_aux(k, n, desert, pos):
-    if (pos >= n):
-        return 0
-
-    if (hyst.tab[pos] != -2):
-        return hyst.tab[pos]
-
-    min_no_saut = -1
-    if (pos + 1 == n or pos + 1 < n and desert[pos + 1] != "#"):
-        min_no_saut = traversee_du_desert_aux(k, n, desert, pos + 1)
-
-    can_saut = True
-    for i in range(1, k + 1):
-        if (pos + i < n and desert[pos + i] == "^"):
-            can_saut = False
-
-    if (pos + k < n and desert[pos + k] == "#"):
-        can_saut = False
-
-    min_saut = -1
-    if (can_saut):
-        min_saut = traversee_du_desert_aux(k, n, desert, pos + k)
-        if (min_saut != -1):
-            min_saut += 1
-
-    if (min_no_saut == -1 or min_saut == -1):
-        hyst.tab[pos] = max(min_saut, min_no_saut)
-    else:
-        hyst.tab[pos] = min(min_saut, min_no_saut)
-
-    # print(pos, hyst.tab[pos])
-    return hyst.tab[pos]
-
-
-def traversee_du_desert(k, n, desert):
-    pos = 0
-    for i in range(0, n):
-        hyst.tab.append(-2)
-        if (desert[i] == "R"):
-            pos = i
-
-    print(traversee_du_desert_aux(k, n, desert, pos))
-
+import heapq
 
 k = int(input())
 n = int(input())
-desert = list(input())
-traversee_du_desert(k, n, desert)
+ch = input()
+
+for _ in range(0, k + 1):
+    ch += ".."
+
+dp = [float('inf') for _ in range(0, len(ch))]
+
+
+def bfs():
+    pile = [
+        (0, 0)
+    ]
+    dp[0] = 0
+    while len(pile) > 0:
+        h, p = heapq.heappop(pile)
+        if p >= n:
+            return h
+        if ch[p + 1] != "#" and h < dp[p + 1]:
+            heapq.heappush(pile, (h, p + 1))
+            dp[p + 1] = h
+        if "^" not in ch[p + 1:p + k + 1] and ch[p + k] != "#" and h + 1 < dp[p + k]:
+            heapq.heappush(pile, (h + 1, p + k))
+            dp[p + k] = h + 1
+
+
+r = bfs()
+if r is None:
+    print(-1)
+else:
+    print(r)
